eeb4561525b52fe508c514f60342e0548d365a2f
Yausell Ruiz <yhauxell@gmail.com>
2018-09-07 09:42:39 -0300

    Adding twitter's bootstrap support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//twitter's bootstrap 4.1.3
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
