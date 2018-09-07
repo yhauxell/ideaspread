12656a7a460d540bc12f6baa29dedeba3f41ce9a
Yausell Ruiz <yhauxell@gmail.com>
2018-09-07 09:50:43 -0300

    Updated bootstrap theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import {Navbar} from './Navbar';
+import { Navbar } from './Navbar';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Navbar></Navbar>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="container-fluid">
+                    <Navbar></Navbar>
+                </div>
+                <div className="container">
+                    <p className="text-center">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
